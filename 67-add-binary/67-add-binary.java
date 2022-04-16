1c1884f445d09b32c32a1b93b28ceb09205f778c
yunamom <hellokin10@gmail.com>
2022-04-16 23:41:01 +0900

    Time: 1 ms (99.99%), Space: 41.3 MB (88.38%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -2,11 +2,22 @@ import java.math.*;
 
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger A_binary = new BigInteger(a, 2);
-        BigInteger B_binary = new BigInteger(b, 2);
+        int A_binary = a.length()-1;
+        int B_binary = b.length()-1;
         
-        BigInteger sum = A_binary.add(B_binary);
+        int sum = 0;
+        StringBuilder builder = new StringBuilder();
         
-        return sum.toString(2);
+        while(sum > 0 || A_binary >= 0 || B_binary >= 0){
+            if(A_binary >= 0){
+                sum += a.charAt(A_binary) - '0'; A_binary--;
+            }
+            if(B_binary >= 0){
+                sum += b.charAt(B_binary) - '0'; B_binary--;
+            }
+            builder.append(sum % 2);
+            sum /= 2;
+        }
+        return builder.reverse().toString();
     }
 }
