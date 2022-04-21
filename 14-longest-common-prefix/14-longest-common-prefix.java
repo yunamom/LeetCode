ae0b3652a3a55d282f867ba108baccd14f552441
yunamom <hellokin10@gmail.com>
2022-04-21 09:03:59 +0900

    Time: 1 ms (80.97%), Space: 42.2 MB (50.60%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        
+        if(strs.length == 0) return "";
+        String answer = strs[0];
+        for(int i=1; i<strs.length; i++){
+            while(strs[i].indexOf(answer) != 0){
+                answer = answer.substring(0,answer.length()-1);
+            }
+        }
+        return answer;
+    }
+}
