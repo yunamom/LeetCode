b72f1c444be2c3550e68350f80e1e39b1a2e95fc
yunamom <hellokin10@gmail.com>
2022-04-21 09:04:55 +0900

    Time: 0 ms (100.00%), Space: 39.8 MB (98.04%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -5,7 +5,7 @@ class Solution {
         String answer = strs[0];
         for(int i=1; i<strs.length; i++){
             while(strs[i].indexOf(answer) != 0){
-                answer = answer.substring(0,answer.length()-1);
+                answer = answer.substring(0, answer.length()-1);
             }
         }
         return answer;
