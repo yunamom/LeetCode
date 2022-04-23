6b5b31f2213bb4f8f3e9c1395891831570e293e5
yunamom <hellokin10@gmail.com>
2022-04-23 09:05:01 +0900

    Time: 2 ms (80.49%), Space: 42.2 MB (32.49%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '[' || c == '{' || c == '(') stack.push(c);
+            else {
+                if (stack.isEmpty()) return false;
+                
+                char popped = stack.pop();
+                if((c == ']' && popped == '[') ||
+                   (c == '}' && popped == '{') ||
+                   (c == ')' && popped == '(')){
+                    continue;
+                }
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
