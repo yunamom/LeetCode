aac30b13bc050e24d15d32744b2569d9209e4aed
yunamom <hellokin10@gmail.com>
2022-04-25 12:47:47 +0900

    Time: 0 ms (100.00%), Space: 42.3 MB (64.42%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,7 +1,6 @@
 class Solution {
     public int strStr(String haystack, String needle) {        
-        if(haystack.equals(needle)) return 0;        
- 
+       
         return haystack.indexOf(needle);
     }
 }
