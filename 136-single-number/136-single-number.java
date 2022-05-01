5d909134268bdbeae3643fa2787e6529254dc9e5
yunamom <hellokin10@gmail.com>
2022-05-01 21:30:23 +0900

    Time: 1 ms (96.07%), Space: 50.5 MB (54.23%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,15 +1,7 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length == 1) return nums[0];       
-        
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for(int key:nums){
-            map.put(key,map.getOrDefault(key, 0)+1);            
-        }
-        for(Integer i : map.keySet()){
-            if(map.get(i) == 1) return i;
-        }
-        return 0;
+        int answer = nums[0];
+        for(int i=1; i<nums.length; i++) answer ^= nums[i];
+        return answer;
     }
 }
