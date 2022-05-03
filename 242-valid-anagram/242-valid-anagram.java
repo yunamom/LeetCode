045cdb006853cb97b67e6343dd3743c6ff765f13
yunamom <hellokin10@gmail.com>
2022-05-03 23:21:35 +0900

    Time: 5 ms (65.15%), Space: 44.9 MB (34.08%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        String ss[] = s.split("");
-		String tt[] = t.split("");
+        if(s.length() != t.length()) return false;
+		char ss[] = s.toCharArray();
+		char tt[] = t.toCharArray();
 		Arrays.sort(ss);
 		Arrays.sort(tt);
-		String sss = "",ttt = "";
-		for(String e:ss) sss+=e;
-		for(String e:tt) ttt+=e;
-		return sss.equals(ttt);
+		for(int i=0; i<ss.length; i++) {
+			if(ss[i] != tt[i]) return false;
+		}		
+		return true;
     }
 }
