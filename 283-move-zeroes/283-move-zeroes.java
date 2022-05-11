53a868c5c86ebeb98d3a7f31f8177efa66d053eb
yunamom <hellokin10@gmail.com>
2022-05-11 16:51:13 +0900

    Time: 2 ms (67.98%), Space: 54.7 MB (48.06%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -4,11 +4,9 @@ class Solution {
         
         for(int i=0; i<nums.length; i++){
             if(nums[i] != 0){
-                int n = nums[i];
-                nums[i] = nums[index];
-                nums[index] = n; index++;
+                nums[index++] = nums[i];
             }
         }
-        
+        while(index < nums.length) nums[index++] = 0;      
     }
 }
