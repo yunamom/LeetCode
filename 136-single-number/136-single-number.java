9b82b088b1271b191eb7737398f7efea88096b06
yunamom <hellokin10@gmail.com>
2022-05-12 19:56:29 +0900

    Time: 1 ms (96.98%), Space: 42.6 MB (80.82%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,17 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        
-        for(int i=0; i<nums.length; i++){
-            int num = nums[i];
-            int cnt = 0;
-            for(int j=0; j<nums.length; j++){
-                if(nums[i] == nums[j]) cnt++;
-                
-                if(cnt == 2) break;
-            }
-            if(cnt == 1) return num;
-        }
-        return nums[0];
+        int answer = nums[0];
+        for(int i=1; i<nums.length; i++){
+            answer ^= nums[i];
+        } 
+        return answer;
     }
 }
